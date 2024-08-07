308dafac1b2e80327394bf7a24d6fd6fd8863aef
Matheus <matheusramosbi.13@gmail.com>
2024-08-06 23:09:51 -0300

    add totalPages variable

--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -1,26 +1,30 @@
 import React from 'react';
 
 const useFetch = () => {
-  const [data, setData] = React.useState(null);
+  const [data, setData] = React.useState([]);
   const [error, setError] = React.useState(null);
   const [loading, setLoading] = React.useState(false);
 
   const request = React.useCallback(async (url, options) => {
     let response;
     let json;
+    let totalPages;
     try {
       setError(null);
       setLoading(true);
       response = await fetch(url, options);
       json = await response.json();
       if (response.ok === false) throw new Error(json.message);
+      if (json.links && json.links.last) {
+        totalPages = +json.links.last.split('=').pop();
+      }
     } catch (err) {
       json = null;
       setError(err.message);
     } finally {
       setData(json.data);
       setLoading(false);
-      return { response, json };
+      return { response, json, totalPages };
     }
   }, []);
 
